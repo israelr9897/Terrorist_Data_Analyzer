9b7c198a5998fae8f9b0899cf1fe07d868d8760c
israelr9897 <israelr9897@gmail.com>
2025-05-15 12:09:50 +0300

    class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SeriesAnalyzer;
+
+namespace SeriesAnalyzer
+{
+    class program()
+    {
+        static void Main(string[] args) //Activates functions according to user selection
+        {
+
+        }
+    }
+}
--- obj/Debug/net9.0/TerroristDataAnalyzer.AssemblyInfo.cs
+++ obj/Debug/net9.0/TerroristDataAnalyzer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TerroristDataAnalyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee8829d7b851f162ae40e4d9772beb39c26791a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TerroristDataAnalyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TerroristDataAnalyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
