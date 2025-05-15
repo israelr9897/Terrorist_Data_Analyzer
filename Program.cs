46264b15df59761ca39aa1b367f0a03903bf30d0
israelr9897 <israelr9897@gmail.com>
2025-05-15 12:11:23 +0300

    no class

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using SeriesAnalyzer;
-
-namespace SeriesAnalyzer
-{
-    class program()
-    {
-        static void Main(string[] args) //Activates functions according to user selection
-        {
-
-        }
-    }
-}
